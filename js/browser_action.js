9bc2477bbb4e69e428ce4609585b408e7aa6db5b
Rodney Blevins <rodney@blevins.nl>
2018-07-16 16:50:32 -0600

    md5, icons

--- js/browser_action.js
+++ js/browser_action.js
@@ -1,17 +1,32 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        url: "https://stackoverflow.com/questions/14349263/creating-a-chrome-extension-which-takes-highlighted-text-on-the-page-and-inserts#answer-14351458",
-        tags: [
-            { name: 'tag' }
-        ],
-        description: null,
+        bookmark: {
+            bookmark_id: null,
+            url: "https://stackoverflow.com/questions/14349263/creating-a-chrome-extension-which-takes-highlighted-text-on-the-page-and-inserts#answer-14351458",
+            tags: [
+                { name: 'tag' }
+            ],
+            description: null,
+            clipping: null
+        },
         newTag: {
             name: null
         },
-        clipping: null,
         message: null
+    },
+    created() {
+        chrome.storage.onChanged.addListener(function(changes, namespace) {
+            for (key in changes) {
+                var storageChange = changes[key];
+                console.log('Storage key "%s" in namespace "%s" changed. ' +
+                          'Old value was "%s", new value is "%s".',
+                          key,
+                          namespace,
+                          storageChange.oldValue,
+                          storageChange.newValue);
+            }
+        })
     },
     watch: {
         message(newVal) {
@@ -41,6 +56,19 @@ var app = new Vue({
                         this.message = "Please make a selection first."
                     }
                 })
+            })
+        },
+        saveBookmark() {
+            if (!this.bookmark.url) {
+                this.message = "URL is a required field."
+                return
+            }
+
+            let url = this.bookmark.url.split("#")
+            this.bookmark.bookmark_id = md5(url[0])
+
+            chrome.storage.sync.get({ key: value }, function() {
+                console.log('Value is set to ' + value);
             })
         }
     }
